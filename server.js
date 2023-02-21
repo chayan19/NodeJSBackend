8cfb48d00f4673f41c123b8fcd8a1c3d6e69244d
ChayanGit <chayan.choudhurypersonal@gmail.com>
2023-02-21 18:56:48 +0530

    F Init

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({path: './config/config.env'});
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(
+    port , 
+    console.log(`Server listening on port ${port} and environment ${process.env.NODE_ENV}`
+      ));
