0c7e8d5545bb9a296577dcd45357d8392a03ee4b
ChayanGit <chayan.choudhurypersonal@gmail.com>
2023-02-22 16:45:10 +0530

    Stable Wed 22

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const dotenv = require('dotenv');
 const basepath = require('./routes/router');
 
@@ -6,6 +7,15 @@ dotenv.config({path: './config/config.env'});
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// mongodb+srv://choudhurychayanpersonal:<password>@mono-chayan-nodejs.z7inq5x.mongodb.net/test
+
+// Use Morgan to log
+if (process.env.NODE_ENV === 'development') {
+    console.log('Starting to log...')
+    app.use(morgan('dev'));
+}
+
 
 //Mount router
 app.use('/api/v1/bootcamps', basepath);
